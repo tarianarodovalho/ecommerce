50fbe63d3446d96afcfe02e88ae38f13ed8c5cea
tarianarodovalho <tariana.rodovalho@gmail.com>
2019-01-19 17:39:08 -0200

    Section 5 - Bootstrap Framework

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 """ from products.views import (
     ProductListView, 
--- src/products/models.py
+++ src/products/models.py
@@ -53,7 +53,7 @@ class Product(models.Model):
     slug        = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=10, default=0)
-    image       = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    image       = models.FileField(upload_to=upload_image_path, null=True, blank=True)
     featured    = models.BooleanField(default=False)
     active      = models.BooleanField(default=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
